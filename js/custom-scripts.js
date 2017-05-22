1e41a83804b24c3bd8c496838e93ce3aa03b7a14
bIropka <biropka@gmail.com>
2017-05-22 22:35:10 +0300

    Initial commit

---
+++ js/custom-scripts.js
@@ -0,0 +1,17 @@
+$(window).ready(function () {
+
+    $('.slider').slick({
+        autoplay: true
+    });
+
+    $('.block-1 .button-blue').click(function() {
+        yaCounter44660062.reachGoal('Get_Coupon');
+        goog_report_conversion('https://www.lyft.com/invite/MAX8008');
+    });
+
+    $('.block-1 .button-blue').click(function() {
+        yaCounter44660062.reachGoal('Get_Free_Ride');
+        goog_report_conversion('https://www.lyft.com/invite/MAX8008');
+    });
+
+});
